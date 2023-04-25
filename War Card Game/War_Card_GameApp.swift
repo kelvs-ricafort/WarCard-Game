81268a3dd51b2c000140b589c68ca5129d49d7b9
Kelvin C. Ricafort <kelvs.r@outlook.com>
2023-04-25 16:32:46 +0800

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Kelvin on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
